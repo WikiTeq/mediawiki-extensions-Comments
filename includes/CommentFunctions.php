29629ccac1701dae05dcaf0aaff091babaf06227
Pavel Astakhov <pastakhov@yandex.ru>
2025-12-12 23:10:40 +0500

    Fix spam detection in CommentFunctions [MW-462]
    - Allow $wgSpamRegex to be an array and check each pattern against the text

--- includes/CommentFunctions.php
+++ includes/CommentFunctions.php
@@ -109,8 +109,18 @@ class CommentFunctions {
 		}
 
 		// Run text through $wgSpamRegex (and $wgSummarySpamRegex if it has been specified)
-		if ( $wgSpamRegex && preg_match( $wgSpamRegex, $text ) ) {
-			return true;
+		if ( $wgSpamRegex ) {
+			if ( is_array( $wgSpamRegex ) ) {
+				foreach ( $wgSpamRegex as $spamRegex ) {
+					if ( preg_match( $spamRegex, $text ) ) {
+						return true;
+					}
+				}
+			} else {
+				if ( preg_match( $wgSpamRegex, $text ) ) {
+					return true;
+				}
+			}
 		}
 
 		if ( $wgSummarySpamRegex && is_array( $wgSummarySpamRegex ) ) {
